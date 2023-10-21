2b761528121f2c6544f80cc0426eb08261de0445
LDebove <59819894+LDebove@users.noreply.github.com>
2023-10-21 19:17:53 +0200

    Add project files.

---
+++ OGL3D/include/OGL3D/Game/OGame.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+
+class OWindow;
+class OGame
+{
+public:
+	OGame();
+	~OGame();
+
+	void run();
+	void quit();
+
+protected:
+	bool m_isRunning = true;
+	std::unique_ptr<OWindow> m_display;
+};
+
