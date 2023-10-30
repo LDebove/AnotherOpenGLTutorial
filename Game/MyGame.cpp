a4d739b7c42907190f261a18e7d77acfe77c00a2
Léo Debove <niearin@gmail.com>
2023-10-30 15:31:33 +0100

    Entity System

---
+++ Game/MyGame.cpp
@@ -0,0 +1,19 @@
+#include "MyGame.h"
+
+MyGame::MyGame()
+{
+}
+
+MyGame::~MyGame()
+{
+}
+
+void MyGame::onCreate()
+{
+	OGame::onCreate();
+	m_player = getEntitySystem()->createEntity<MyPlayer>();
+}
+
+void MyGame::onUpdate(f32 deltaTime)
+{
+}
